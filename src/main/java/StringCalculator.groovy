95bd92c3b9c152bfcd6b378def72b45929906f26
Klaus Bayrhammer <bayrhammer.klaus@gmail.com>
2013-01-03 21:28:42 +0100

    removed InnerStringCalculator

--- src/main/java/StringCalculator.groovy
+++ src/main/java/StringCalculator.groovy
@@ -3,93 +3,90 @@
  */
 class StringCalculator {
 
-    static int add(String input) {
-        new InnerCalculator(input).add()
+    private static final String DEFAULT_DELIMITER = "[\n,]"
+
+    private String delimiter
+    private String numbersWithDelimiters
+    private final String input;
+    private ArrayList parsedNumbers = []
+
+    public StringCalculator(String input) {
+        this.input = input
     }
 
-    static class InnerCalculator {
+    static int add(String input) {
+        new StringCalculator(input).add()
+    }
 
-        private static final String DEFAULT_DELIMITER = "[\n,]"
+    int add() {
+        if (isInputEmpty())
+            return 0
 
-        private String delimiter
-        private String numbersWithDelimiters
-        private final String input;
-        private ArrayList parsedNumbers = []
+        handleDelimiter()
+        parseNumbers()
+        calculateSum()
+    }
 
-        public InnerCalculator(String input) {
-            this.input = input
+    private boolean isInputEmpty() {
+        input.isEmpty()
+    }
+
+    private void handleDelimiter() {
+        if (hasCustomDelimiter()) {
+            parseCustomDelimiter()
+        } else {
+            useDefaultDelimiter()
         }
+    }
 
-        int add() {
-            if (isInputEmpty())
-                return 0
+    private boolean hasCustomDelimiter() {
+        input.startsWith("//")
+    }
 
-            handleDelimiter()
-            parseNumbers()
-            calculateSum()
+    private void useDefaultDelimiter() {
+        numbersWithDelimiters = input
+        delimiter = DEFAULT_DELIMITER
+    }
+
+    private void parseCustomDelimiter() {
+        def endOfFirstLine = input.indexOf("\n")
+        List customDelimitors = input.substring(3, endOfFirstLine - 1).split("\\]\\[")
+        delimiter = customDelimitors.join("|")
+        numbersWithDelimiters = input.substring(endOfFirstLine + 1)
+    }
+
+    private List parseNumbers() {
+        numbersWithDelimiters.split(delimiter).each { token ->
+            parsedNumbers << new ParsedNumber(token)
         }
+    }
 
-        private boolean isInputEmpty() {
-            input.isEmpty()
+    private int calculateSum() {
+        int result = 0;
+        List negativeValues = []
+        parsedNumbers.each { number ->
+            if (!number.ignored)
+                result += number.value
+            if (number.illegal)
+                negativeValues << number.value
         }
-
-        private void handleDelimiter() {
-            if (hasCustomDelimiter()) {
-                parseCustomDelimiter()
-            } else {
-                useDefaultDelimiter()
-            }
+        if (!negativeValues.isEmpty()) {
+            throw new IllegalArgumentException(String.format('negatives not supported (%s)', negativeValues.join(",")))
         }
+        result
+    }
 
-        private boolean hasCustomDelimiter() {
-            input.startsWith("//")
-        }
+    static class ParsedNumber {
+        final int value;
+        final boolean ignored;
+        final boolean illegal;
 
-        private void useDefaultDelimiter() {
-            numbersWithDelimiters = input
-            delimiter = DEFAULT_DELIMITER
-        }
-
-        private void parseCustomDelimiter() {
-            def endOfFirstLine = input.indexOf("\n")
-            List customDelimitors = input.substring(3, endOfFirstLine-1).split("\\]\\[")
-            delimiter = customDelimitors.join("|")
-            numbersWithDelimiters = input.substring(endOfFirstLine+1)
-        }
-
-        private List parseNumbers() {
-            numbersWithDelimiters.split(delimiter).each { token ->
-                parsedNumbers << new ParsedNumber(token)
-            }
-        }
-
-        private int calculateSum() {
-            int result = 0;
-            List negativeValues = []
-            parsedNumbers.each { number ->
-                if (!number.ignored)
-                    result += number.value
-                if (number.illegal)
-                    negativeValues << number.value
-            }
-            if (!negativeValues.isEmpty()) {
-                throw new IllegalArgumentException(String.format('negatives not supported (%s)', negativeValues.join(",")))
-            }
-            result
-        }
-
-        static class ParsedNumber {
-            final int value;
-            final boolean ignored;
-            final boolean illegal;
-
-            ParsedNumber(String strValue) {
-                this.value = Integer.valueOf(strValue)
-                if (value > 1000)
-                    ignored = true;
-                if (value < 0)
-                    illegal = true;
-            }
+        ParsedNumber(String strValue) {
+            this.value = Integer.valueOf(strValue)
+            if (value > 1000)
+                ignored = true;
+            if (value < 0)
+                illegal = true;
         }
     }
 }
